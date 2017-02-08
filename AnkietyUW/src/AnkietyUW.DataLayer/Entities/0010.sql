1d52e8055a9a331a00a306d095b606b4cb67946e
ArkadyPL <a.ryszewski@outlook.com>
2017-02-08 19:09:35 +0100

    sql for db with file

---
+++ AnkietyUW/src/AnkietyUW.DataLayer/Entities/0010.sql
@@ -0,0 +1,42 @@
+﻿CREATE TABLE [dbo].[Answer] (
+    [Id] CHAR (36) NOT NULL PRIMARY KEY,
+    [DateTime] DATE NOT NULL, 
+    [TestId] CHAR(36) NOT NULL, 
+    [SeriesNumber] INT NOT NULL, 
+    [UserId] CHAR(36) NULL, 
+);
+
+CREATE TABLE [dbo].[Test]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstQuestionAddSeconds] INT NOT NULL, 
+    [SecondQuestionAddSeconds] INT NOT NULL, 
+    [ThirdQuestionAddSeconds] INT NOT NULL, 
+    [FourthQuestionAddSeconds] INT NOT NULL, 
+    [CompletedSeriesCounter] INT NOT NULL, 
+    [TimeToFillTestAddSeconds] INT NOT NULL
+);
+
+CREATE TABLE [dbo].[Secret]
+(
+	[Id] CHAR(36) NOT NULL PRIMARY KEY, 
+    [UserId] CHAR(36) NOT NULL, 
+    [SeriesNumber] INT NOT NULL
+);
+
+CREATE TABLE [dbo].[TestTime]
+(
+	[Id] CHAR(36) NOT NULL PRIMARY KEY, 
+    [DateTime] DATE NOT NULL, 
+    [TestId] CHAR(36) NOT NULL
+);
+
+CREATE TABLE [dbo].[User]
+(
+	[Id] CHAR(36) NOT NULL PRIMARY KEY, 
+    [Key] CHAR(256) NOT NULL, 
+    [EmailAddress] CHAR(256) NOT NULL, 
+    [Active] BIT NOT NULL, 
+    [Sex] INT NOT NULL, 
+    [TestId] INT NOT NULL
+);
